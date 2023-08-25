9ec7bafe12d585c01df88de1ea1e775edf151631
juanploaizan <juanp.loaizan@uqvirtual.edu.co>
2023-08-25 17:39:11 -0500

    build: initialized project

---
+++ src/test/java/com/powercoffee/PowercoffeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powercoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowercoffeeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
