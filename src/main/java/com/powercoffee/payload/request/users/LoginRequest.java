1d0966077bb143ba9d5846f1352ad6e01f421092
Sebastian Burgos Puentes <sebastian.burgosp@uqvirtual.edu.co>
2023-10-22 12:50:09 -0500

    fix: gradle-wrapper.properties conflict

--- src/main/java/com/powercoffee/payload/request/users/LoginRequest.java
+++ src/main/java/com/powercoffee/payload/request/users/LoginRequest.java
@@ -1,6 +1,5 @@
 package com.powercoffee.payload.request.users;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
